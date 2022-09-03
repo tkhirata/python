02e000d62f00f703b403f5385e1d96e0429507d6
Tyler Hirata <tylerhirata@gmail.com>
2022-09-03 11:35:18 -0700

    Update palindrome_verifier.py
    Created again without using regex and string splicing to reverse order or strings.

--- palindrome_verifier/palindrome_verifier.py
+++ palindrome_verifier/palindrome_verifier.py
@@ -1,20 +1,49 @@
-import re
+'''list of palindromes and non-palindromes'''
+strings = ["Anna", "madam", "apples", "mango", "gravy", "repaper", "taco cat", "top spot", "Don't nod.", "I did, did I?", "Eva, can I see bees in a cave?"]
 
-strings = ["racecar", "apples", "ana2", "a santa at+  nasa", "meSSenger", "the !cAt in!! the hat", "avId diva", "Amy, must I jujitsu my ma??", "Ah, Satan sees Natasha!!"]
+'''empty list for storing letters of string'''
+separated_string = []
 
+'''empty list for storing letters of string reversed'''
+reversed_separated_string = []
 
+'''counter for returning original strings in final output'''
+i = 0
+
+'''takes string out of list'''
 for string in strings:
-	'''Using regex to return only letters in a list'''
-	remove_characters = re.findall(r"[aA-zZ]+",string)
+	'''removes any white spaces and makes everything lower case'''
+	string = string.replace(' ','').lower()
 	
-	'''Using the join method to merge elements in the list'''
-	remove_characters = ''.join(remove_characters).replace(" ","").lower()
+	'''takes each letter of string and adds it to a list'''
+	for letter in string:
+		'''Checks to see if a letter is a letter'''
+		character_check = letter.isalpha()
 
-	'''This reverses the order of remove_characters'''
-	flippled_remove_characters = remove_characters[::-1].replace(" ","")
+		'''if character is a letter it adds to the list'''
+		if character_check == True:
+			separated_string.append(letter)
 
-	'''Conditional check to see if the reversed string matches unreversed string'''
-	if remove_characters == flippled_remove_characters:
-		print(f"{string} is a palindrome!")
+			'''takes letter and add it to front of reversed string list'''
+			reversed_separated_string.insert(0,letter)
+
+	'''combines all the elements together in separated string list'''
+	combined_string = "".join(separated_string)
+
+	'''combines all the elements together in reversed string list'''
+	combined_string_reversed = "".join(reversed_separated_string)
+	
+	'''final conditional check the compares reversed string with non-reversed'''
+	if combined_string == combined_string_reversed:
+		print(f"\"{strings[i]}\" is a palindrome!")
+		'''clears out list so elements aren't combined with next string'''
+		separated_string.clear()
+		reversed_separated_string.clear()
 	else:
-		print(f"{string} is not a palindrome!")
+		print(f"\"{strings[i]}\" is not a palindrome!")
+		separated_string.clear()
+		reversed_separated_string.clear()
+
+	i = i + 1
+
+
